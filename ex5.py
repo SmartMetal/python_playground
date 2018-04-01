59616131a59c2ac6c6ac318e2113a4f59e7b265b
SmartMetal <newfacechangealot@gmail.com>
2018-04-01 10:19:50 -0700

    just adding more

---
+++ ex5.py
@@ -0,0 +1,18 @@
+i = 'Anon E. Mouse'
+t = 25
+j = 69 #inches
+d = 180 #pounds
+x = 'Blue'
+w = 'Brown'
+z= 'multicolor'
+
+print "Let's talk about %s." % i
+print "He's %d inches tall." % t
+print "He's %d pounds heavy." % j
+print "Actually he's kind of heavy."
+print "He's got %s eyes and %s hair." % (x, z)
+print "His teeth are a gnarly %s." % w
+
+#This line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (t, j, d, t + j + d)
+ 
